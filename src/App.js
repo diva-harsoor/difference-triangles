d88d67a03c73fc0149c983b39fdbab2e1018c318
Diva Harsoor <divaharsoor@gmail.com>
2024-04-10 14:28:43 -0700

    Fixed non-unique keys for children Created large circular cells and adjusted formatting

--- src/App.js
+++ src/App.js
@@ -1,51 +1,65 @@
 import { useState, useEffect } from 'react';
 
-function Square({ value }) {
-    return <button className="square">{value}</button>;
+// Circle with arrows
+function Cell({ index, value, arrows, onClick }) {
+  const sideClassName = `rhombus center ${arrows && arrows.includes(0) ? 'purple' : ''}`;
+
+  const belowClassName = `rhombus ${
+    arrows && arrows.includes(1) ? 'tilt-left purple' : ''
+  } ${arrows && arrows.includes(-1) ? 'tilt-right purple' : ''}`;
+
+  return (
+      <div key={index} className="cell-container">
+        <div className="cell-row">
+          <button className="circle" onClick={onClick}>{value}</button>
+          <div className={sideClassName}></div>
+        </div>
+        <div className={belowClassName}></div>
+      </div>
+    );
   }
 
 export default function Board() {
-  const renderSquare = (value) => (
-    <Square key={value} value={value} />
-  );
-
-  // Combine, refactor
-  const renderColDot = () => (
-    <div className="coldot"></div>
-  )
-
-  const renderRowDot = () => (
-    <div className="rowdot"></div>
-  )
+  const [cellArrows, setCellArrows] = useState([]);
+  
+  useEffect(() => {
+    const initialCellArrows = [
+      [],
+      [1],
+      [0],
+      [],
+      [0],
+      [0,-1],
+    ];
+    setCellArrows(initialCellArrows);
+  }, []);
 
   const renderCells = (n_rows=3) => {
     let board = [];
     let length = (n_rows * (n_rows + 1))/2;
     let cell_index = length;
     for (let row_i = n_rows; row_i > 0; row_i--) {
-      const rowSquares = Array(row_i).fill(null);
-      const rowDots = Array(row_i).fill(null);
-      const leftPadding = Array(n_rows - row_i).fill(<div className="empty-square" />);
+      const rowCells = [];
+      const leftPadding = Array(n_rows - row_i)
+        .fill(<div className="empty-square" />)
+        .map((_, index) => <div key={`padding-${row_i}-${index}`}> </div>);
       for (let col_i = row_i - 1; col_i >= 0; col_i--) {
         cell_index--;
-        rowSquares.push(renderSquare(cell_index + 1));
-        rowSquares.push(renderColDot(cell_index + 1));
-        rowDots.push(renderRowDot(cell_index + 1))
+        rowCells.push(
+          <Cell 
+            key={`cell-${cell_index}`}
+            value={cell_index + 1}
+            arrows={cellArrows[cell_index]}
+          />
+        );
       }
-      const squareRow = (
-        <div key={row_i} className="board-row centered-row" >
+      const cellRows = (
+        <div key={`row-${row_i}`} className="board-row centered-row" >
           {leftPadding}
-          {rowSquares}
+          {rowCells}
         </div>
       );
-      const dotRow = (
-        <div key={row_i} className="board-row centered-row" >
-        {leftPadding}
-        {rowDots}
-      </div>
-      );
-      board.push(squareRow);
-      board.push(dotRow);
+      board.push(cellRows);
     }
     return board;
   };
