a9a7543acd654666140e1b184d679cd22a0a1cb0
Diva Harsoor <divaharsoor@gmail.com>
2024-04-10 18:17:31 -0700

    Added click handlers for cells Added success logic

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function Cell({ index, value, arrows, onClick }) {
   return (
       <div key={index} className="cell-container">
         <div className="cell-row">
-          <button className="circle" onClick={onClick}>{value}</button>
+          <button className="circle" onClick={() => onClick(index)}>{value}</button>
           <div className={sideClassName}></div>
         </div>
         <div className={belowClassName}></div>
@@ -21,18 +21,34 @@ function Cell({ index, value, arrows, onClick }) {
 
 export default function Board() {
   const [cellArrows, setCellArrows] = useState([]);
+  const [cells, setCells] = useState([]);
   
   useEffect(() => {
-    const initialCellArrows = [
-      [],
-      [1],
-      [0],
-      [],
-      [0],
-      [0,-1],
-    ];
+    const initialCellArrows = [[],[1],[0],[],[0],[0,-1]];
     setCellArrows(initialCellArrows);
+
+    const populateCells = (n_rows=3) => {
+      let length = (n_rows * (n_rows + 1))/2;
+      let cell_index = length;
+      let tempCells = Array(length).fill(null);
+      for (let row_i = n_rows; row_i > 0; row_i--) {
+        for (let col_i = row_i - 1; col_i >= 0; col_i--) {
+          cell_index--;
+          // tempCells[cell_index] = cell_index + 1;
+          tempCells[cell_index] = 1;
+        }
+      }
+      setCells(tempCells);
+    };
+
+    populateCells();
   }, []);
+
+  function handleClick(i) {
+    const tempCells = cells.slice();
+    tempCells[i] += 1;
+    setCells(tempCells);
+  }
 
   const renderCells = (n_rows=3) => {
     let board = [];
@@ -40,16 +56,16 @@ export default function Board() {
     let cell_index = length;
     for (let row_i = n_rows; row_i > 0; row_i--) {
       const rowCells = [];
-      const leftPadding = Array(n_rows - row_i)
-        .fill(<div className="empty-square" />)
-        .map((_, index) => <div key={`padding-${row_i}-${index}`}> </div>);
+      const leftPadding = Array(n_rows - row_i).fill(null);
       for (let col_i = row_i - 1; col_i >= 0; col_i--) {
         cell_index--;
+        const handleCellClick = (index) => () => handleClick(index);
         rowCells.push(
           <Cell 
             key={`cell-${cell_index}`}
-            value={cell_index + 1}
+            value={cells[cell_index]}
             arrows={cellArrows[cell_index]}
+            onClick={handleCellClick(cell_index)}
           />
         );
       }
@@ -64,5 +80,33 @@ export default function Board() {
     return board;
   };
 
-  return <>{renderCells()}</>;
+  let status = "You can do it!";
+  if (checkSuccess(cells, 3)) {
+    status = "Congrats! You got it!";
+  }
+
+  return (
+    <>
+      <div className="centered-row">{status}</div>
+      {renderCells()}
+    </>
+  );
+}
+
+function checkSuccess(cells, n_rows) {
+  console.log("entered checkSuccess");
+  let diff_i = 0;
+  let subt_i = 1;
+  for (let row_i=1; row_i<=n_rows-1; row_i++) {
+    for (let col_i=row_i; col_i>0; col_i--) {
+      console.log(`${diff_i}, ${subt_i}, ${subt_i+1}`);
+      if (Math.abs(cells[subt_i + 1] - cells[subt_i]) != cells[diff_i]) {
+        return false;
+      }
+      diff_i++;
+      subt_i++;
+    }
+    subt_i++;
+  }
+  return true;
 }
