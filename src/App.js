f8700394a9f0d4f2b9512102ffa73882bde52ce3
Diva Harsoor <divaharsoor@gmail.com>
2024-04-16 23:57:26 -0700

    Refactored to create Keypad function

--- src/App.js
+++ src/App.js
@@ -126,8 +126,71 @@ function checkSuccess(cells, n_rows) {
   return "Congrats! You got it!";
 }
 
-function NumberBank() {
-  
+function Keypad({ numbers, scratch, onNumPress, onScratchPress }) {
+  function handleNumClick(i) {
+    onNumPress(i+1);
+  }
+
+  function handleScratchClick() {
+    onScratchPress(!scratch);
+  }
+
+  const renderNumberBank = () => {
+    console.log(`numbers.length: ${numbers.length}`);
+    if (numbers.length == 0) {
+      console.log("hit numbers.length == 0");
+      return [];
+    }
+    let halflength = Math.floor(numbers.length/2);
+    let factor_short = halflength;
+    let min_diff = numbers.length;
+    for (let i = 2; i < halflength; i++) {
+      let factor = numbers.length/i;
+      if ((factor % 1) == 0) {
+        let complement = numbers.length/factor;
+        if (Math.abs(complement - factor) < min_diff) {
+          min_diff = Math.abs(complement - factor);
+          factor_short = factor < complement ? factor : complement;
+        }
+      }
+    }
+
+    let bank = [];
+    let factor_long = numbers.length/factor_short;
+    let counter = 0;
+    for (let row_i=0; row_i<factor_short; row_i++) {
+      const rowNums = [];
+      for (let col_i=0; col_i<factor_long; col_i++) {
+        const handleEachNumClick = (index) => () => handleNumClick(index);
+        rowNums.push(
+          <Number
+            key={`number-${counter+1}`}
+            index={counter}
+            value={numbers[counter]}
+            onClick={handleEachNumClick(counter)}
+          />
+        );
+        counter++;
+      }
+      const numberRows = (
+        <div key={`numrow-${row_i}`} className="centered-row">
+          {rowNums}
+        </div>
+      );
+      bank.push(numberRows);
+    }
+    bank.push(
+      <button key={`scratch`} className="scratch-button" onClick={handleScratchClick}>
+        {scratch ? "Disable scratch" : "Enable scratch"}
+      </button>
+    );
+    return bank;
+  }
+  return (
+    <>
+      {renderNumberBank()}
+    </>
+  );
 }
 
 export default function Game() {
@@ -178,59 +241,12 @@ export default function Game() {
     setScratchArrays(nextScratchArrays);
   }
 
-  function handleNumClick(i) {
-    setSelectedNumber(i+1);
+  function handleNumPress(nextSelectedNumber) {
+    setSelectedNumber(nextSelectedNumber);
   }
 
-  function handleScratchClick() {
-    setScratch(!scratch);
-  }
-
-  const renderNumberBank = () => {
-    if (numbers.length == 0) {
-      return [];
-    }
-    else {
-    }
-    let halflength = Math.floor(numbers.length/2);
-    let factor_short = halflength;
-    let min_diff = numbers.length;
-    for (let i = 2; i < halflength; i++) {
-      let factor = numbers.length/i;
-      if ((factor % 1) == 0) {
-        let complement = numbers.length/factor;
-        if (Math.abs(complement - factor) < min_diff) {
-          min_diff = Math.abs(complement - factor);
-          factor_short = factor < complement ? factor : complement;
-        }
-      }
-    }
-
-    let bank = [];
-    let factor_long = numbers.length/factor_short;
-    let counter = 0;
-    for (let row_i=0; row_i<factor_short; row_i++) {
-      const rowNums = [];
-      for (let col_i=0; col_i<factor_long; col_i++) {
-        const handleEachNumClick = (index) => () => handleNumClick(index);
-        rowNums.push(
-          <Number
-            key={`number-${counter+1}`}
-            index={counter}
-            value={numbers[counter]}
-            onClick={handleEachNumClick(counter)}
-          />
-        );
-        counter++;
-      }
-      const numberRows = (
-        <div key={`numrow-${row_i}`} className="centered-row">
-          {rowNums}
-        </div>
-      );
-      bank.push(numberRows);
-    }
-    return bank;
+  function handleScratchPress(nextScratch) {
+    setScratch(nextScratch);
   }
 
   return (
@@ -244,8 +260,7 @@ export default function Game() {
       <div>
         <Board numRows={numRows} cells={cells} clueCells={clueCells} cellArrows={cellArrows} selectedNumber={selectedNumber} scratchArrays={scratchArrays} scratch={scratch} onPlay={handlePlay} />
       </div>
-      {renderNumberBank()}
-      <button onClick={handleScratchClick}>{scratch ? "Disable scratch" : "Enable scratch"}</button>
+      <Keypad numbers={numbers} scratch={scratch} onNumPress={handleNumPress} onScratchPress={handleScratchPress}/>
     </>
   );
 }
